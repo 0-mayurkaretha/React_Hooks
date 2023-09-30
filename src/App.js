e378fb5fd57ac3c6c7049ded70bc30360775b83d
0-mayurkaretha <0.mayurkaretha@gmail.com>
2023-09-30 11:45:19 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import UseState from "./components/UseState";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div>
+          <button><Link to="/">Home</Link></button> | &nbsp;
+          <button><Link to="/UseState">useState Hook</Link></button> | &nbsp;
+          
+          <br />
+        </div>
+        <Routes>
+          <Route path="/UseState" element={<UseState />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/UseState.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react"
+
+const UseState = () => {
+
+  //simple
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  //Object
+  const [name, setName] = useState({ fName: '', lName: '' })
+
+  //Array
+  const [items, setItems] = useState([]);
+  const addItem = () => {
+    setItems([...items, { 
+      id: items.length, 
+      value: Math.floor(Math.random() * 10 ) + 1
+    }])
+  }
+
+  return (
+    <div>
+      <br /><br />
+      ============ Simple ===========
+      <br /><br />
+      {/* //====== Unsafe ======= */}
+      button1 :
+      <button onClick={() => setCount1(count1 + 1)}>count++</button> | {count1}
+      <br /><br />
+
+      {/*========= safe ======= */}
+      button2 :
+      <button onClick={() => setCount2(0)}>reset</button> | {count2}
+      <br /><br />
+
+      button3 :
+      <button onClick={() => setCount2(prevCount => prevCount + 1)}>inc</button> | {count2}
+      <br /><br />
+
+      button4 :
+      <button onClick={() => setCount2(prevCount => prevCount - 1)}>dec</button> | {count2}
+
+      <br /><br /><br />
+      ============ Object ===========
+      <br /><br />
+
+      <form>
+        <input type="text" placeholder="your fname" value={name.fName} onChange={e => setName({ ...name, fName: e.target.value })} />
+        <input type="text" placeholder="your lname" value={name.lName} onChange={e => setName({ ...name, lName: e.target.value })} />
+        <h2>your first name is - {name.fName}</h2>
+        <h2>your first name is - {name.lName}</h2>
+      </form>
+
+      <br /><br />
+      ============ Array ===========
+      <br /><br />
+      <div>
+        <button onClick={addItem}>Add a number</button>
+        <ul>
+          {
+            items.map(item => 
+              <li key={item.id}> {item.value} </li>
+            )
+          }
+        </ul>
+      </div>
+
+    </div>
+  )
+};
+
+export default UseState;
